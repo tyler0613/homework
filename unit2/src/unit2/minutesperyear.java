697b8262d3d9c3e9dc4995fda4c622bff70b6f32
hatch <hatch@tyler.hsd1.pa.comcast.net>
2022-08-31 16:39:40 -0400

    hhh

---
+++ unit2/src/unit2/minutesperyear.java
@@ -0,0 +1,16 @@
+package unit2;
+public class minutesperyear {
+
+	public static void main(String[] args) {
+		Scanner years = new Scanner(System.in);
+		System.out.print("How many years");
+		double years = myObj.nextDouble;
+		
+		System.Out.Println(years);
+				
+		
+				
+				
+	}
+
+}
---
+++ unit2/src/unit2/velocity.java
@@ -0,0 +1,18 @@
+package unit2;
+
+public class velocity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner mass = new Scanner(System.in);
+		System.out.println("What is the mass");
+		double mass = myObj.nextDouble;
+		Scanner velocity = new Scanner(System.in);
+		System.out.println("What is the velocity");
+		double velocity = myObj.nextDouble;
+				
+		System.out.println(double mass * double velocity + "Is the momentum");
+	}
+
+}
